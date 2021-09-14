853a6d28f2abfe43f0daaa69c294d17a521ec9c5
mxTuhin <tuhinmridha11@gmail.com>
2021-09-15 00:35:14 +0600

    Updated Rows Script

--- Assets/Script/Rows.cs
+++ Assets/Script/Rows.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Rows : MonoBehaviour
 {
@@ -32,9 +34,75 @@ public class Rows : MonoBehaviour
         timeInterval = 0.025f;
         for (int i = 0; i < 30; ++i)
         {
-            if(transform)
+            if (transform.position.y <= -3.5f)
+                transform.position = new Vector2(transform.position.x, 0.70f);
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.30f);
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        randomValue = Random.Range(60, 100);
+        switch (randomValue % 3)
+        {
+            case 1:
+                randomValue += 2;
+                break;
+            case 2:
+                randomValue += 1;
+                break;
+        }
+
+        for (int i = 0; i < randomValue; ++i)
+        {
+            if (transform.position.y <= -3.5f)
+                transform.position = new Vector2(transform.position.x, 0.70f);
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.30f);
+
+            if (i > Mathf.RoundToInt(randomValue * 0.25f))
+                timeInterval = 0.05f;
+            if (i > Mathf.RoundToInt(randomValue * 0.55f))
+                timeInterval = 0.1f;
+            if (i > Mathf.RoundToInt(randomValue * 0.75f))
+                timeInterval = 0.15f;
+            if (i > Mathf.RoundToInt(randomValue * 0.95f))
+                timeInterval = 0.2f;
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        if (transform.position.y == -3.5f)
+        {
+            stoppedSlot = "Diamond";
+        }
+        else if (transform.position.y == -2.9f)
+        {
+            stoppedSlot = "Crown";
+        }
+        else if (transform.position.y == -2.3f)
+        {
+            stoppedSlot = "Melon";
+        }
+        else if (transform.position.y == -1.7f)
+        {
+            stoppedSlot = "Bar";
+        }else if (transform.position.y == -1.1f)
+        {
+            stoppedSlot = "Seven";
+        }else if (transform.position.y == -0.5f)
+        {
+            stoppedSlot = "Cherry";
+        }else if (transform.position.y == 0.1f)
+        {
+            stoppedSlot = "Lemon";
+        }else if (transform.position.y == 0.7f)
+        {
+            stoppedSlot = "Diamond";
         }
     }
+
+    private void OnDestroy()
+    {
+        GameControl.HandlePulled -= StartRotating;
+    }
+
     // Update is called once per frame
     void Update()
     {
